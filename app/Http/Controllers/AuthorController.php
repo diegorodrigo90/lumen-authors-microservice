675c75267ad42e1cf93c81ce69e24fd7208f92b4
Diego Rodrigo <diegorodrigo90@gmail.com>
2021-02-13 16:06:55 -0300

    created AuthorController

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthorController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Return a list of authors.
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Create a new author.
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display details about one author.
+     *
+     * @return illuminate\Http\Response
+     */
+    public function show($authorId)
+    {
+        //
+    }
+
+    /**
+     * Update details about one author.
+     *
+     * @return illuminate\Http\Response
+     */
+    public function update($authorId)
+    {
+        //
+    }
+
+    /**
+     * Delete one author.
+     *
+     * @return illuminate\Http\Response
+     */
+    public function destroy($authorId)
+    {
+        //
+    }
+}
